e6954798843dd5e7da4512d2f0903e7c7722105a
Serhiy Smyslov <serhiy.smyslov@gmail.com>
2021-04-13 13:33:04 +0300

    Add DataBase PostgreSQL

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+
 # Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=50)
+    date = models.DateField()
+    text = models.TextField()
+    image = models.ImageField(upload_to='blog_media/')
